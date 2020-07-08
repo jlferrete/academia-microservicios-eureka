ac3a53f6ea12b26f0e53062c4f4443bdb49e5e6d
JlFerrete <joseluisferrete@gmail.com>
2020-07-08 11:31:34 +0200

    Initial commit

---
+++ src/main/java/com/formacionbdi/microservicios/app/eureka/MicroserviciosEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosEurekaApplication.class, args);
+	}
+
+}
